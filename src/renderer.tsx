0b5f0ea1df3f0108b7494be59777c113be54692e
tegnike <o3nike.teg.14@gmail.com>
2025-01-02 16:59:08 +0100

    favicon設置

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -9,6 +9,10 @@ export const renderer = jsxRenderer(({ children, title }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="My portfolio and blog site" />
         <link href="/static/styles/globals.css" rel="stylesheet" />
+        <link rel="icon" href="/static/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
       </head>
       <body class="bg-white dark:bg-gray-900">
         {children}
