5098dfeddff964d9a19741d08c217ec92ffe1d8e
ニケちゃん / nikechan <o3nike.teg.14@gmail.com>
2025-04-30 21:12:25 +0200

    スキの色変更

--- src/components/TechBlog.tsx
+++ src/components/TechBlog.tsx
@@ -143,11 +143,11 @@ export const TechBlog = ({ noteArticles, shuffledImageNumbers }: TechBlogProps) 
                     <h3 className="text-white font-bold text-lg mb-2 line-clamp-2 truncate-2-lines">
                       {article.title}
                     </h3>
-                    <div className="flex items-center text-gray-400">
+                    <div className="flex items-center text-[#EB4667]">
                       <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path>
                       </svg>
-                      <span>{article.like_count} スキ</span>
+                      <span>{article.like_count}</span>
                     </div>
                   </a>
                 )
